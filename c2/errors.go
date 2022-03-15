2309f3b2f6ae43f2a5f3c0040b477cab1c32b32b
Rafael <pepe1615@hotmail.com>
2022-03-15 08:47:53 -0300

    Errors added

---
+++ c2/errors.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type MyError struct {
+	When time.Time
+	What string
+}
+
+// Aqui implementamos la interfaz de 'error'
+func (e *MyError) Error() string {
+	return fmt.Sprintf("at %v, %s",
+		e.When, e.What)
+}
+
+// La interfaz 'error' es una interfaz que ya esta definida dentro de go
+// Usualmente los errores se devuelven como segundo parametro
+// nil en caso que no haya error y sino se devuelve entero
+// esto FUERZA el manejo de excepciones y errores
+func Sqrt(x float64) (float64, error) {
+	if x > 0 {
+		return x / x, nil
+	} else {
+		return 0, &MyError{
+			time.Now(),
+			"Number cant be negative",
+		}
+	}
+}
+
+func main() {
+	// Aca imprimo mis errores
+	fmt.Println(Sqrt(2))
+	//Aca imprimo mi error custom
+	fmt.Println(Sqrt(-2))
+}
---
+++ c2/interfaces.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// Asi se declara una interfaz en go
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+// Estp quiere decir que T implementa la interfaz I
+// no se tiene que declarar explicitamente
+func (t T) M() {
+	fmt.Println(t.S)
+}
+
+func main() {
+	var i I = T{"hello"}
+	i.M()
+}
